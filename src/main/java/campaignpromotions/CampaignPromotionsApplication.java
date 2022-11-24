f90a93679ed9f65ecfe6ad8727bfbfa1527fa84f
dcalderon-osm <dcalderon@osm-soft.io>
2022-11-23 23:50:25 -0300

    Se agrego la dependencia springfox para manejar swagger

---
+++ src/main/java/campaignpromotions/CampaignPromotionsApplication.java
@@ -0,0 +1,13 @@
+package campaignpromotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignPromotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignPromotionsApplication.class, args);
+	}
+
+}
